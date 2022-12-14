dc734cf810db03b27d046f1acbd307c3c9e7ff3c
the-name-romel <racopra@sorsu.edu.ph>
2022-12-14 18:57:18 +0800

    modify title and footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="w-full h-[100px] bg-primary flex text-center items-center justify-center text-[22px]">
-      <h1>French Chezka Gazo 2022</h1>
+      <h1>Jayvee Genavia 2022</h1>
     </div>
   );
 }
